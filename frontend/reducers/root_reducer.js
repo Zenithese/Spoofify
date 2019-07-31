19a1b09e6459ee912be3f74a5820b0e03d4a48c3
Justin <jandersen770@gmail.com>
2019-07-31 08:51:49 -0400

    Add dependencies and root reducer

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -1,15 +1,17 @@
 import { combineReducers } from 'redux';
 
 import entities from './entities_reducer';
-import ui from './ui_reducer';
-import session from './session_reducer';
+import {sessionReducer} from './sessions_reducer';
 import errors from './errors_reducer';
+import session_errors from './session_errors_reducer'
+import users from './users_reducer'
 
 const rootReducer = combineReducers({
     entities,
-    session,
-    ui,
+    session: sessionReducer,
     errors,
+    session_errors,
+    users,
 });
 
 export default rootReducer;
