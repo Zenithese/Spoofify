ac18e28f1c5aee7de2ee3c6b6558fb7a4ef480e0
Justin <jandersen770@gmail.com>
2019-08-01 14:15:30 -0400

    Edit for heroku

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -1,14 +1,14 @@
 class Playlist < ApplicationRecord
-    validates :title, presence: true
+    # validates :title, presence: true
 
-    belongs_to :user
+    # belongs_to :user
 
-    has_many :follows
+    # has_many :follows
 
-    has_one :playlist_id,
-        foreign_key: :playlist_id,
+    # has_one :playlist_id,
+    #     foreign_key: :playlist_id,
 
-    has_many :songs,
-        through: :playlist_id,
-        source: :song
+    # has_many :songs,
+    #     through: :playlist_id,
+    #     source: :song
 end
