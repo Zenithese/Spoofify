d3c306c9a81fc25ef6a9324e741ed77733b7704d
Justin <jandersen770@gmail.com>
2019-08-02 20:49:15 -0400

    Fit it

--- app/models/playlist_song.rb
+++ app/models/playlist_song.rb
@@ -1,6 +1,6 @@
 class PlaylistSong < ApplicationRecord
     # validates :song_id, :playlist_id, precense: true
 
-    belongs_to :playlist,
-    belongs_to :song,
+    belongs_to :playlist
+    belongs_to :song
 end
